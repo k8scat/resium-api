43eb48c7bf69b3c8a16eb0e5df639b12ba4aea2b
hsowan <hsowan.v@gmail.com>
2020-01-28 19:16:17 +0800

    Update auto_login.py [ci-skip]

--- auto_login.py
+++ auto_login.py
@@ -5,23 +5,25 @@
 @date: 2020/1/17
 
 """
-import time
-
 from selenium import webdriver
-from urllib.parse import unquote
 
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
+
+    selenium_server = 'http://selenium:4444/wd/hub'
+    driver = webdriver.Remote(command_executor=selenium_server)
     try:
-        driver.get("https://passport.csdn.net/v1/register/authorization?authType=github")
-        driver.find_element_by_id("login_field").clear()
-        driver.find_element_by_id("login_field").send_keys("hsowan-me")
-        driver.find_element_by_id("password").click()
+        linkedin_login_url = 'https://www.linkedin.com/login'
+        driver.get(linkedin_login_url)
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("hsowan.me@gmail.com")
         driver.find_element_by_id("password").clear()
         driver.find_element_by_id("password").send_keys("holdon7868")
-        driver.find_element_by_name("commit").click()
-        driver.get('https://download.csdn.net/')
-        time.sleep(60)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+
+        print(driver.page_source)
+        print(type(driver.page_source))
+        print(driver.page_source.count('万华松'))
     finally:
         driver.close()
