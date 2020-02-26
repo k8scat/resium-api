c6d6ad0133065583f745fb3e2cbf10fd6b842df9
hsowan <hsowan.v@gmail.com>
2020-02-26 22:36:18 +0800

    Fix cors for browser ext

---
+++ csdnbot/middleware.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: hsowan <hsowan.me@gmail.com>
+@date: 2020/2/26
+
+"""
+from django.utils.deprecation import MiddlewareMixin
+
+
+class CorsMiddleware(MiddlewareMixin):
+    """
+    解决cors问题
+
+    Todo: django-cors-headers 没有在请求头添加 Access-Control-Allow-Origin
+
+    """
+
+    def process_response(self, request, response):
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+
+
