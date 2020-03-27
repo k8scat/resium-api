00221a3d14533bdb79d8a17f2e7e9888b8ba80c3
hsowan <hsowan.v@gmail.com>
2020-03-28 03:45:49 +0800

    Update

--- downloader/apis/coolq.py
+++ downloader/apis/coolq.py
@@ -19,6 +19,7 @@ from rest_framework.request import Request
 @api_view(['POST'])
 def receive(request: Request):
     if request.method == 'POST':
+        logging.info(request.headers)
         x_sig = request.headers.get('X-Signature', None)
         qq = request.headers.get('X-Self-ID', None)
         if x_sig and qq != settings.CQ_QQ:
@@ -28,5 +29,4 @@ def receive(request: Request):
                 logging.info('ok')
                 # 请求确实来自于插件
                 logging.info(request.data)
-
         return HttpResponse(status=204)
