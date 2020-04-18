a586144eb249ac79d2220753bc00e9afd4af827d
hsowan <hsowan.v@gmail.com>
2020-04-19 00:32:18 +0800

    Update

--- downloader/models.py
+++ downloader/models.py
@@ -23,6 +23,7 @@ class User(Base):
     github_id = models.IntegerField(default=None, null=True)
     gitee_id = models.IntegerField(default=None, null=True)
     baidu_openid = models.CharField(max_length=100, default=None, null=True)
+    dingtalk_openid = models.CharField(max_length=100, default=None, null=True)
 
     # 废弃的字段
     email = models.EmailField(verbose_name='邮箱', default=None, null=True)
