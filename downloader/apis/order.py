75fa00ac9242ee760d056b4043990ff81ee4726c
hsowan <hsowan.v@gmail.com>
2020-06-18 08:16:26 +0800

    Update

--- downloader/apis/order.py
+++ downloader/apis/order.py
@@ -94,8 +94,9 @@ def list_orders(request):
     return JsonResponse(dict(code=requests.codes.ok, orders=OrderSerializers(orders, many=True).data))
 
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 def mp_pay_notify(request):
+    logging.info('*********************************7777')
     logging.info(request.data)
     logging.info(request.POST)
     return HttpResponse('')
