279407c69f2437c1863d017f6b25554227ab1aa9
hsowan <hsowan.v@gmail.com>
2020-07-11 18:27:05 +0800

    Update

---
+++ download_mbzj.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: hsowan <hsowan.me@gmail.com>
+@date: 2020/7/11
+
+"""
+
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resium.settings.prod')
+django.setup()
+
+import json
+from bs4 import BeautifulSoup
+from django.conf import settings
+from downloader.models import Resource
+from time import sleep
+import requests
+from downloader.utils import get_random_ua
+
+
+def parse_resources(url):
+    headers = {
+        'user-agent': get_random_ua(),
+        'referer': 'http://www.cssmoban.com/'
+    }
+    with requests.get(url, headers) as r:
+        soup = BeautifulSoup(r.content.decode(), 'lxml')
+        for item in soup.select('ul.thumbItem.large li>a'):
+            resource_url = 'http://www.cssmoban.com' + item.get('href')
+            resources[resource_url] = 0
+
+        next_page_url = soup.find('td', attrs={'id': 'pagelist'}).select('a.next')[1].get('href')
+        if soup.find('td', attrs={'id': 'pagelist'}).select('a.next')[1].get('href') != 'javascript:;':
+            next_page_url = 'http://www.cssmoban.com/cssthemes/' + next_page_url
+            parse_resources(next_page_url)
+
+
+if __name__ == '__main__':
+    # resources = {}
+    # parse_resources(url='http://www.cssmoban.com/cssthemes/index.shtml')
+    # with open('mbzj.json', 'w') as f:
+    #     f.write(json.dumps(resources))
+
+    with open('mbzj.json', 'r') as f:
+        resources = json.loads(f.read())
+
+    for k, v in resources.items():
+        if Resource.objects.filter(url=k).count() == 1:
+            print('已存在')
+            continue
+
+        headers = {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2NjY2NjYifQ.hcP4NzJUIigurnA0c1iXmE0H-kYaIogIz-iusKzf1jc1sL6VGm3ejnATQIPVNaB-oAWJSX1_KMKK9_KxvWCGvA'
+        }
+        data = {
+            'url': k,
+            'point': settings.MBZJ_POINT,
+            't': 'url'
+        }
+        with requests.post('https://api.resium.cn/download/', headers=headers, json=data) as r:
+            print(r.content)
+
+    with open('mbzj.json', 'w') as f:
+        f.write(json.dumps(resources))
--- downloader/apis/resource.py
+++ downloader/apis/resource.py
@@ -849,54 +849,46 @@ class MbzjResource(BaseResource):
             'referer': self.url,
             'user-agent': get_random_ua()
         }
-        # 如果cookies失效，r.json()会抛出异常
         with requests.get(download_url, headers=headers, data=data) as r:
-            try:
-                resp = r.json()
-                if resp['code'] == 0:
-                    # 更新用户积分
-                    self.user.point -= point
-                    self.user.used_point += point
-                    self.user.save()
-                    PointRecord(user=self.user, used_point=point,
-                                comment='下载模板之家模板', url=self.url,
-                                point=self.user.point).save()
+            resp = r.json()
+            if resp['code'] == 0:
+                # 更新用户积分
+                self.user.point -= point
+                self.user.used_point += point
+                self.user.save()
+                PointRecord(user=self.user, used_point=point,
+                            comment='下载模板之家模板', url=self.url,
+                            point=self.user.point).save()
 
-                    download_url = 'http://down.qfpffmp.cn' + resp['data']
-                    self.filename = resp['data'].split('/')[-1]
-                    self.filepath = os.path.join(self.save_dir, self.filename)
-                    with requests.get(download_url, stream=True) as download_resp:
-                        if download_resp.status_code == requests.codes.OK:
-                            with open(self.filepath, 'wb') as f:
-                                for chunk in download_resp.iter_content(chunk_size=1024):
-                                    if chunk:
-                                        f.write(chunk)
+                download_url = 'http://down.qfpffmp.cn' + resp['data']
+                self.filename = resp['data'].split('/')[-1]
+                self.filepath = os.path.join(self.save_dir, self.filename)
+                with requests.get(download_url, stream=True) as download_resp:
+                    if download_resp.status_code == requests.codes.OK:
+                        with open(self.filepath, 'wb') as f:
+                            for chunk in download_resp.iter_content(chunk_size=1024):
+                                if chunk:
+                                    f.write(chunk)
 
-                            return requests.codes.ok, '下载成功'
+                        return requests.codes.ok, '下载成功'
 
-                        ding('[模板之家] 下载失败',
-                             error=download_resp.text,
-                             uid=self.user.uid,
-                             download_account_id=self.account.id,
-                             resource_url=self.url,
-                             logger=logging.error,
-                             need_email=True)
-                        return requests.codes.server_error, '下载失败'
-                else:
                     ding('[模板之家] 下载失败',
-                         error=r.text,
+                         error=download_resp.text,
                          uid=self.user.uid,
+                         download_account_id=self.account.id,
                          resource_url=self.url,
                          logger=logging.error,
                          need_email=True)
                     return requests.codes.server_error, '下载失败'
-            except JSONDecodeError:
-                ding('[模板之家] Cookies失效',
+            else:
+                ding('[模板之家] 下载失败',
+                     error=r.text,
                      uid=self.user.uid,
                      resource_url=self.url,
                      logger=logging.error,
                      need_email=True)
                 return requests.codes.server_error, '下载失败'
+
 
     def get_filepath(self):
         status, result = self.__download()
