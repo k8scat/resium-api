edf1c1c1c9e7468b6ead649258e9c5f4faf5f06d
hsowan <hsowan.v@gmail.com>
2020-08-09 02:55:03 +0800

    Update: 飞书机器人测试

--- downloader/apis/feishu.py
+++ downloader/apis/feishu.py
@@ -9,7 +9,7 @@ import logging
 
 from django.conf import settings
 from django.http import JsonResponse
-from django.http.response import HttpResponseBase
+from django.http.response import HttpResponseBase, HttpResponse
 from rest_framework.decorators import api_view
 
 from downloader import utils
@@ -23,7 +23,9 @@ def bot(request):
     logging.info(type(data))
     if data:
         token = data.get('token', '')
+        # 验证事件来源
         if token == settings.FEISHU_APP_VERIFICATION_TOKEN:
+            # 事件类型
             feishu_request_type = data.get('type', '')
             if feishu_request_type == 'url_verification':
                 challenge = data.get('challenge', '')
@@ -32,24 +34,21 @@ def bot(request):
 
             elif feishu_request_type == 'event_callback':
                 # 获取事件内容和类型，并进行相应处理，此处只关注给机器人推送的消息事件
-                event = data.get("event")
-                if event.get("type", "") == "message":
+                event = data.get('event')
+                if event.get('type', '') == 'message':
                     # 此处只处理 text 类型消息，其他类型消息忽略
-                    msg_type = event.get("msg_type", "")
-                    if msg_type != "text":
-                        return HttpResponseBase()
+                    msg_type = event.get('msg_type', '')
+                    if msg_type == "text":
+                        logging.info(f'Got message: {event.get("text_without_at_bot", "")}')
 
-                    # 调用发消息 API 之前，先要获取 API 调用凭证：tenant_access_token
-                    access_token = utils.feishu_get_tenant_access_token()
-                    if not access_token:
-                        return HttpResponseBase()
-
-                    # 机器人 echo 收到的消息
-                    utils.feishu_send_message(access_token, event.get("open_id"), 'Got!')
-                    return HttpResponseBase()
+                        # 调用发消息 API 之前，先要获取 API 调用凭证：tenant_access_token
+                        access_token = utils.feishu_get_tenant_access_token()
+                        if access_token:
+                            # 机器人 echo 收到的消息
+                            utils.feishu_send_message(access_token, event.get('open_id'), 'Got!')
+                            return HttpResponseBase()
         else:
             ding(message='feishu verification token not match, token = ' + token,
                  logger=logging.warning)
-            return HttpResponseBase()
-    else:
-        return HttpResponseBase()
+
+    return HttpResponse('')
