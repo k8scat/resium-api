263b0fe6f6be37f1e330728605e67ade1bb72c96
hsowan <1583096683@qq.com>
2020-10-30 08:14:30 +0800

    Update: 使用指定的csdn账号解析文章

--- downloader/apis/article.py
+++ downloader/apis/article.py
@@ -40,7 +40,7 @@ def parse_csdn_article(request):
 
     article_url = article_url.split('?')[0]
     try:
-        article = Article.objects.get(url=article_url)
+        article = Article.objects.get(id=7)
         return JsonResponse(dict(code=200, article=ArticleSerializers(article).data))
 
     except Article.DoesNotExist:
